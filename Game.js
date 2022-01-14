32f12bf853298d65b74bc9c59f41ec493b25922e
Eric Matlock <ermatlock@gmail.com>
2022-01-14 12:05:38 -0500

    Update game class properties

--- Game.js
+++ Game.js
@@ -1,10 +1,10 @@
 class Game {
 	constructor() {
-		this.player1 = new Player('one', 'icon');
-		this.player2 = new Player('two', 'icon');
+		this.player1 = new Player('one', 'put sprite name here');
+		this.player2 = new Player('two', 'put sprite name here');
 		this.positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 	}
 
-	
+
 }
