b22e3da04ae5c3175b8441fbe9733ecea4568fe3
Eric Matlock <ermatlock@gmail.com>
2022-01-14 00:40:08 -0500

    Add game properties

--- Game.js
+++ Game.js
@@ -1,6 +1,10 @@
 class Game {
 	constructor() {
-		this.player1 =
-		this.player2 =
+		this.player1 = new Player('one', 'icon');
+		this.player2 = new Player('two', 'icon');
+		this.positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 	}
+
+	
 }
