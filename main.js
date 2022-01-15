b38d5bf0e3bce497f40dd351dada1007bbf84053
Eric Matlock <ermatlock@gmail.com>
2022-01-15 14:22:48 -0500

    Transfer functions to Game class as methods

--- main.js
+++ main.js
@@ -1,48 +1,7 @@
-positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-p1 = []
-p2 = []
-
-function checkEach(player) {
-	checkForWin(player, [1, 2, 3])
-	checkForWin(player, [4, 5, 6])
-	checkForWin(player, [7, 8, 9])
-	checkForWin(player, [1, 4, 7])
-	checkForWin(player, [2, 5, 8])
-	checkForWin(player, [3, 6, 9])
-	checkForWin(player, [1, 5, 9])
-	checkForWin(player, [3, 5, 7])
+determineTurn() {
+	if ()
 }
 
-function checkForWin(player, winState) {
-	var matches = [];
-	for(var i = 0; i < player.length; i++) {
-		for(var j = 0 ; j < winState.length; j++) {
-			if(player[i] === winState[j]) {
-				matches.push(player[i]);
-			}
-		}
-	}
-	if(matches.length === 3) {
-
-		console.log('WIN!');
-	}
-}
-
-function playNumber(player, number) {
-	for(var i = 0; i < positions.length; i++) {
-		if (number === positions[i]) {
-			splice(i, 1);
-			player.push(number);
-			checkEach(player);
-		}
-	}
-}
-
-function gameReset() {
-	positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-	p1 = []
-	p2 = []
-}
 
 // 1 2 3
 // 4 5 6
