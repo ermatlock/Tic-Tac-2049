048968e71eeac781277b4166cbb1f2a1c3f23b07
Eric Matlock <ermatlock@gmail.com>
2022-01-16 17:00:19 -0500

    Update game logic

--- main.js
+++ main.js
@@ -8,23 +8,23 @@ var c1 = document.getElementById('c1');
 var c2 = document.getElementById('c2');
 var c3 = document.getElementById('c3');
 
-a1.addEventListener('click', function () {takeTurn(1)});
-a2.addEventListener('click', function () {takeTurn(2)});
-a3.addEventListener('click', function () {takeTurn(3)});
-b1.addEventListener('click', function () {takeTurn(4)});
-b2.addEventListener('click', function () {takeTurn(5)});
-b3.addEventListener('click', function () {takeTurn(6)});
-c1.addEventListener('click', function () {takeTurn(7)});
-c2.addEventListener('click', function () {takeTurn(8)});
-c3.addEventListener('click', function () {takeTurn(9)});
+a1.addEventListener('click', function () {takeTurn('a1')});
+a2.addEventListener('click', function () {takeTurn('a2')});
+a3.addEventListener('click', function () {takeTurn('a3')});
+b1.addEventListener('click', function () {takeTurn('b1')});
+b2.addEventListener('click', function () {takeTurn('b2')});
+b3.addEventListener('click', function () {takeTurn('b3')});
+c1.addEventListener('click', function () {takeTurn('c1')});
+c2.addEventListener('click', function () {takeTurn('c2')});
+c3.addEventListener('click', function () {takeTurn('c3')});
 
 
 function takeTurn(position) {
+
 	console.log('thisisthecurrentplayer ' + currentGame.currentPlayer)
-	if (currentGame.positions.length === 9) {
-		choosePosition(player1, position);
-	}
-	if (currentGame.currentPlayer === 1) {
+	// if (currentGame.positions.length === 9) {
+	// 	choosePosition(player1, position);
+	 if (currentGame.currentPlayer === 1) {
 		console.log('takeTurn player1')
 		choosePosition(player1, position);
 	} else {
@@ -40,10 +40,17 @@ function choosePosition(player, position) {
 			console.log(currentGame.positions)
 			currentGame.positions.splice(i, 1);
 			player.choices.push(position);
+			placeToken(player, position);
 			checkEach(player);
-			switchPlayers()
+			switchPlayers();
 		}
 	}
+}
+
+function placeToken(player, position) {
+	console.log(position)
+	console.log(player)
+	position.innerText = player.token 
 }
 
 function switchPlayers() {
@@ -58,14 +65,14 @@ function switchPlayers() {
 }
 
 function checkEach(player) {
-	this.checkForWin(player, [1, 2, 3])
-	this.checkForWin(player, [4, 5, 6])
-	this.checkForWin(player, [7, 8, 9])
-	this.checkForWin(player, [1, 4, 7])
-	this.checkForWin(player, [2, 5, 8])
-	this.checkForWin(player, [3, 6, 9])
-	this.checkForWin(player, [1, 5, 9])
-	this.checkForWin(player, [3, 5, 7])
+		checkForWin(player, ['a1', 'a2', 'a3'])
+		checkForWin(player, ['b1', 'b2', 'b3'])
+		checkForWin(player, ['c1', 'c2', 'c3'])
+		checkForWin(player, ['a1', 'b1', 'c1'])
+		checkForWin(player, ['a2', 'b2', 'c2'])
+		checkForWin(player, ['a3', 'b3', 'c3'])
+		checkForWin(player, ['a1', 'b2', 'c3'])
+		checkForWin(player, ['c1', 'b2', 'a3'])
 }
 
 function checkForWin(player, winState) {
@@ -74,6 +81,7 @@ function checkForWin(player, winState) {
 		for(var j = 0 ; j < winState.length; j++) {
 			if(player.choices[i] === winState[j]) {
 				matches.push(player.choices[i]);
+				console.log('current' + matches)
 			}
 		}
 	}
@@ -81,71 +89,23 @@ function checkForWin(player, winState) {
 		console.log('winning ' + matches)
 		player.wins ++
 		console.log(`${player.id} WON!`)
-		nextGame();
-		return `${player.id} WON!`;
+		currentGame.resetGame();
+		// return `${player.id} WON!`;
 	}
 	if (currentGame.positions.length === 0) {
 		console.log('player1 ' + player1.choices)
 		console.log('player2 ' + player2.choices)
 		console.log('DRAW!')
-		nextGame();
-		return 'DRAW!'
+		currentGame.resetGame();
+		// return 'DRAW!'
 	}
 }
 
-function nextGame() {
-	console.log('nextGame')
-	player1.choices = [];
-	player2.choices = [];
-	currentGame.positions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-	currentGame.currentPlayer = 1
-	console.log(currentGame.currentPlayer)
-}
-
-
-
-
-// 1 2 3
-// 4 5 6
-// 7 8 9
-
-// Start player 1
-// Player 1 play any number
-// if (gameBoard.includes[i])
-// gameBoard.splice(i)
-// p1.push(i)
-// if p1.includes(winState1-8)
-// win!
-// else switch p2
-
-// Player 2 play any number that still exists in gameBoard
-// if (gameBoard.includes[i])
-// gameBoard.splice(i)
-// p2.push(i)
-// if p1.includes(winState1-8)
-// win!
-// else switch p1
-
-// Player 1 play any number that still exists in gameBoard
-// if (gameBoard.includes[i])
-// gameBoard.splice(i)
-// p2.push(i)
-// if p1.includes(winState1-8)
-// win!
-// else switch p2
-
-// if p1.includes(winstate)
-// WIN!
-// p1.wins ++
-
-// if p1.includes(winstate)
-// WIN!
-// p2.wins ++
-
-// if gameBoard = [] and !p1.includes(winstate) and !p2.includes(winstate)
-// DRAW!
-
-
-
-// game reset:
-// gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// function nextGame() {
+// 	console.log('nextGame')
+// 	// player1.choices = [];
+// 	// player2.choices = [];
+// 	// currentGame.positions = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
+// 	// currentGame.currentPlayer = 1
+// 	// console.log(player1.choices + player2.choices + currentGame.positions + currentGame.currentPlayer)
+// }
