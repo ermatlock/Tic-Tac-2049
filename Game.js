b74d0f8d130b0759235c1e29c51e03cae3b61701
Eric Matlock <ermatlock@gmail.com>
2022-01-16 22:24:59 -0500

    Update player arguments

--- Game.js
+++ Game.js
@@ -1,9 +1,10 @@
 class Game {
 	constructor(player1, player2) {
-		this.player1 = new Player(player1, 'X');
-		this.player2 = new Player(player2, 'O');
+		this.player1 = new Player(1, 'X');
+		this.player2 = new Player(2, 'O');
 		this.positions = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
 		this.currentPlayer = 1;
+		this.startingPlayer = 1;
 
 	}
 
@@ -11,7 +12,6 @@ class Game {
 		this.player1.choices = []
 		this.player2.choices = []
 		this.positions = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
-		this.currentPlayer = 1;
 	}
 
 	// player1.choices = [];
