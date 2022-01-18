321ea501ba73a3ea58249c8034ab596b93b4eb6a
Eric Matlock <ermatlock@gmail.com>
2022-01-17 21:20:10 -0500

    Add audio asset classes

--- data.js
+++ data.js
@@ -2,3 +2,7 @@ var currentGame = new Game('player1', 'player2');
 var player1 = currentGame.player1;
 var player2 = currentGame.player2;
 var playButtonStatus = false;
+
+var backgroundMusic = new Audio('./assets/music/Tic-Tac-2049-theme.mp3');
+var bleep = new Audio('./assets/sfx/compu-bleep.wav');
+var draw = new Audio('./assets/sfx/draw.mp3');
