7f024ddf928c792475cdcbc3d217e0ef9d2b82e4
Eric Matlock <ermatlock@gmail.com>
2022-01-22 22:45:17 -0500

    Refactor remove redundant classes

--- data.js
+++ data.js
@@ -8,13 +8,3 @@ var bleep = new Audio("./assets/sfx/compu-bleep.wav");
 var draw = new Audio("./assets/sfx/draw.mp3");
 var player1Start = new Audio("./assets/sfx/player-1-start.mp3");
 var player2Start = new Audio("./assets/sfx/player-2-start.mp3");
-
-// var winState1 = ["a1", "a2", "a3"];
-// var winState2 = ["b1", "b2", "b3"];
-// var winState3 = ["c1", "c2", "c3"];
-// var winState4 = ["a1", "b1", "c1"];
-// var winState5 = ["a2", "b2", "c2"];
-// var winState6 = ["a3", "b3", "c3"];
-// var winState7 = ["a1", "b2", "c3"];
-// var winState8 = ["c1", "b2", "a3"];
-// var positions = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
