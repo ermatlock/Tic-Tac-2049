8d17c91c83d703533fbef34f978482c12cfbaa0e
Eric Matlock <ermatlock@gmail.com>
2022-01-22 22:45:52 -0500

    Rafactor

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /*~~~~~~~~~~~~~~QUERY SELECTORS~~~~~~~~~~~~~~~~~~*/
-var gameInfo = document.getElementById("gameInfo");
-var player1Box = document.getElementById("p1");
-var player2Box = document.getElementById("p2");
-var player1Wins = document.getElementById("player1Wins");
-var player2Wins = document.getElementById("player2Wins");
-var ticTacBox = document.getElementById("ticTacBox");
-var playButton = document.getElementById("playButton");
+const gameInfo = document.getElementById("gameInfo");
+const player1Box = document.getElementById("p1");
+const player2Box = document.getElementById("p2");
+const player1Wins = document.getElementById("player1Wins");
+const player2Wins = document.getElementById("player2Wins");
+const ticTacBox = document.getElementById("ticTacBox");
+const playButton = document.getElementById("playButton");
 
 /*~~~~~~~~~~~~~~EVENT LISTENERS~~~~~~~~~~~~~~~~~~*/
 ticTacBox.addEventListener("click", function(e) {
@@ -50,96 +50,15 @@ function takeTurn(e) {
 	}
 }
 
-// function choosePosition(player, position) {
-// 	for (var i = 0; i < currentGame.positions.length; i++) {
-// 		if (position === currentGame.positions[i]) {
-// 			currentGame.positions.splice(i, 1);
-// 			player.choices.push(position);
-// 			placeToken(player, position);
-// 			bleep.play();
-// 			checkWinOrDraw(player);
-// 			changePlayer();
-// 		}
-// 	}
-// }
-
-// function changePlayer() {
-// 	if (currentGame.currentPlayer === 1) {
-// 		updateGameInfo("Turn: Player 2");
-// 		add(player2Box, "active");
-// 		remove(player1Box, "active");
-// 		currentGame.currentPlayer = 2;
-// 	} else if (currentGame.currentPlayer === 2) {
-// 		updateGameInfo("Turn: Player 1");
-// 		add(player1Box, "active");
-// 		remove(player2Box, "active");
-// 		currentGame.currentPlayer = 1;
-// 	}
-// }
-
 function placeToken(player, position) {
-	eval(position)["innerHTML"] = `<h1 class="glitch">${player.token}</h1>`;
-}
-
-// function checkWinOrDraw(player) {
-// 	for (var k = 1; k < 9; k++) {
-// 		winState = eval(`winState${k}`);
-// 		checkWinStates(player, winState);
-// 	}
-// 	if (player.winner) {
-// 		logWin(player);
-// 	} else {
-// 		checkForDraw();
-// 	}
-// }
-//
-// function checkWinStates(player, winState) {
-// 	var matches = [];
-// 	for (var i = 0; i < player.choices.length; i++) {
-// 		for (var j = 0; j < winState.length; j++) {
-// 			if (player.choices[i] === winState[j]) {
-// 				matches.push(player.choices[i]);
-// 			}
-// 		}
-// 	}
-// 	if (matches.length === 3) {
-// 		player.winner = true;
-// 	}
-// }
-
-function logWin(player) {
-	player.addWins();
-	console.log(`Player ${player.id} WINS!`)
-	updateGameInfo(`Player ${player.id} WINS!`);
-	ticTacBox.classList.add("block-clicks");
-	var playerWins = new Audio(`./assets/sfx/player-${player.id}-wins.mp3`);
-	playerWins.play();
-	currentGame.nextGame();
-}
-
-function checkForDraw() {
-	if (currentGame.currentPositions.length === 0) {
-		updateGameInfo("DRAW!");
-		ticTacBox.classList.add("block-clicks");
-		draw.play();
-		currentGame.nextGame();
-	}
+	var currentPosition = eval(position);
+	currentPosition.innerHTML = `<h1 class="grid-token glitch">${player.token}</h1>`;
 }
 
 function updateWins() {
 	player1Wins.innerText = player1.wins;
 	player2Wins.innerText = player2.wins;
 }
-
-// function nextGame() {
-// 	setTimeout(function() {
-// 		currentGame.resetGame();
-// 		currentGame.switchStartingPlayer();
-// 		ticTacBox.classList.remove("block-clicks");
-// 		clearBoard();
-// 		updateWins();
-// 	}, 3500);
-// }
 
 function switchPlayerView(activePlayerId, inactivePlayerId) {
 	updateGameInfo(`Turn: Player ${activePlayerId}`);
