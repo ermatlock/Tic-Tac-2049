61d816d78e60ace79c8dda35ecfb815d51c45b49
Eric Matlock <ermatlock@gmail.com>
2022-04-14 17:00:13 -0400

    Update Player class with tokenName

--- Player.js
+++ Player.js
@@ -1,7 +1,8 @@
 class Player {
-	constructor(id, token) {
+	constructor(id, token, tokenName) {
 		this.id = id;
 		this.token = token;
+		this.tokenName = tokenName;
 		this.choices = [];
 		this.wins = 0;
 		this.winner = false;
